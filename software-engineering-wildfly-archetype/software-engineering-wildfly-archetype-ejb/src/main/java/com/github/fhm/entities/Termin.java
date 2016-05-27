0c6271205d2e3d2fe6d93641e82f1c15ed9e1c3a
TweakerBell <amaydad@gmail.com>
2016-05-27 20:10:49 +0200

    Annotationen in der Klasse Termin hinzugefügt

--- software-engineering-wildfly-archetype/software-engineering-wildfly-archetype-ejb/src/main/java/com/github/fhm/entities/Termin.java
+++ software-engineering-wildfly-archetype/software-engineering-wildfly-archetype-ejb/src/main/java/com/github/fhm/entities/Termin.java
@@ -1,5 +1,26 @@
 package com.github.fhm.entities;
 
-public class Termin {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Termin implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id @GeneratedValue
+	private int id;
+	private Date timestamp;
+	private int anzahl;
+	
+	
+	public Termin(){}
+	
+	
+	public int getId() {
+		return id;
+	}
+	
 }
