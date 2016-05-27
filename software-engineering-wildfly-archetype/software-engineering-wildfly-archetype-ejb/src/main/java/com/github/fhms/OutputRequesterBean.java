87b214e49462a21ce166bcc3e570c546e89f1ba7
TweakerBell <amaydad@gmail.com>
2016-05-27 21:41:53 +0200

    Für das Subsystem "messaging-activemq" eine neue Queue deklariert
    Stateless Session Bean OutputRequesterBean hinzugefügt

---
+++ software-engineering-wildfly-archetype/software-engineering-wildfly-archetype-ejb/src/main/java/com/github/fhms/OutputRequesterBean.java
@@ -0,0 +1,24 @@
+package com.github.fhms;
+
+
+import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+
+
+/**
+ * Session Bean implementation class OutputRequesterBean
+ */
+@Stateless
+@LocalBean
+public class OutputRequesterBean {
+
+	
+	
+	   
+    public OutputRequesterBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
