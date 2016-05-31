c1369e5df7b931861ac63397bb2f79075c124c46
TweakerBell <amaydad@gmail.com>
2016-05-31 19:02:56 +0200

    Namen geändert

---
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/dao/ErstiHelferDAOLocal.java
@@ -0,0 +1,8 @@
+package com.github.fhm.dao;
+
+import javax.ejb.Local;
+
+@Local
+public class ErstiHelferDAOLocal {
+
+}
---
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/interfaces/ErstiHelferService.java
@@ -0,0 +1,9 @@
+package com.github.fhm.interfaces;
+/**
+ * @author Amayda Dominguez
+ * 
+ * */
+public interface ErstiHelferService {
+
+	public String getName(String name);
+}
