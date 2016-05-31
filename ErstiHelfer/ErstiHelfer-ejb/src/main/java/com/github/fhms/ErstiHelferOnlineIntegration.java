66639aee767397e066679b4d6e834aa783331431
Sipsip <malde.e@web.de>
2016-05-31 15:21:30 +0200

    rename Projects to ErstiHelfer

--- ErstiHelfer/ErstiHelfer-ejb/src/main/java/com/github/fhms/ErstiHelferOnlineIntegration.java
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/com/github/fhms/ErstiHelferOnlineIntegration.java
@@ -5,7 +5,6 @@ import javax.ejb.Stateless;
 import javax.jws.WebService;
 
 import com.github.fhm.dao.ErstiHelferDAOLocal;
-import com.github.fhm.interfaces.ErstiHelferService;
 import com.github.fhm.util.DtoAssembler;
 
 /**
