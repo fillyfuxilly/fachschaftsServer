8cc98422c75fc95ed93acaae591ca684849dff48
TweakerBell <amaydad@gmail.com>
2016-06-01 21:45:45 +0200

    OutputRequesterBean hinzugefügt

---
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/erstihelfer/OutputRequesterBean.java
@@ -0,0 +1,20 @@
+package de.erstihelfer.erstihelfer;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class OutputRequesterBean
+ */
+@Stateless
+@LocalBean
+public class OutputRequesterBean {
+
+    /**
+     * Default constructor. 
+     */
+    public OutputRequesterBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
