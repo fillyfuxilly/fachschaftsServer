5c0cd22acba5509f43199d6861332d6445bf5490
TweakerBell <amaydad@gmail.com>
2016-06-02 15:08:39 +0200

    ändenrungen

---
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/dto/DataTransferObject.java
@@ -0,0 +1,10 @@
+package de.erstihelfer.dto;
+
+import java.io.Serializable;
+
+public abstract class DataTransferObject implements Serializable {
+
+	private static final long serialVersionUID = -7048071682623786158L;
+
+
+}
---
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/interfaces/ErstiHelferService.java
@@ -0,0 +1,9 @@
+package de.erstihelfer.interfaces;
+/**
+ * @author Amayda Dominguez
+ * 
+ * */
+public interface ErstiHelferService {
+
+	public String getName(String name);
+}
