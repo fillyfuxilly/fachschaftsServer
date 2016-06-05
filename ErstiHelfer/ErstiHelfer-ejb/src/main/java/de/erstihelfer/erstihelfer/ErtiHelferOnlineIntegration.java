90a8dc42c728df105c58d979fe53fef1f4f8afa3
TweakerBell <amaydad@gmail.com>
2016-06-05 18:52:45 +0200

    webContext erstihelfer

--- ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/erstihelfer/ErtiHelferOnlineIntegration.java
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/erstihelfer/ErtiHelferOnlineIntegration.java
@@ -7,6 +7,7 @@ import javax.ejb.TransactionManagementType;
 import javax.jws.WebService;
 
 import org.jboss.logging.Logger;
+import org.jboss.ws.api.annotation.WebContext;
 
 import de.erstihelfer.dao.ErstiHelferDAOLocal;
 import de.erstihelfer.entities.ErstiHelferSession;
@@ -24,6 +25,7 @@ import de.erstihelfer.util.DtoAssembler;
 @Stateless 
 
 @WebService
+@WebContext(contextRoot="/erstihelfer")
 @TransactionManagement(TransactionManagementType.CONTAINER)
 public class ErtiHelferOnlineIntegration {
 	private static final Logger logger = Logger.getLogger(ErtiHelferOnlineIntegration.class);
