5845af4ed81e0d3dd4ddf6401124000ab8f6e619
TweakerBell <amaydad@gmail.com>
2016-06-06 15:32:51 +0200

    gets bei UserTO geschrieben

--- ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/dto/UserTO.java
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/dto/UserTO.java
@@ -4,17 +4,28 @@ public class UserTO {
 
 	private String username;
 	private int groupNr;
-	
-	public UserTO(){}
-	public UserTO(String username){
-		
-		this.username=username;
+
+	public UserTO() {
 	}
-	
+
+	public UserTO(String username) {
+
+		this.username = username;
+	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	public void setGroupNr (int groupNr) {
-		this.groupNr = groupNr;}
-	
+
+	public void setGroupNr(int groupNr) {
+		this.groupNr = groupNr;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public int getGroupNr() {
+		return groupNr;
+	}
 }
