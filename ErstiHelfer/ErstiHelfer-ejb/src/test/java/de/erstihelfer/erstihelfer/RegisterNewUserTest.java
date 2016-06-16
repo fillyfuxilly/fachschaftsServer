66f907626a1f12a76bd00b2eb7912293192de4e3
TweakerBell <amaydad@gmail.com>
2016-06-16 18:59:26 +0200

    Test auskommentiert

--- ErstiHelfer/ErstiHelfer-ejb/src/test/java/de/erstihelfer/erstihelfer/RegisterNewUserTest.java
+++ ErstiHelfer/ErstiHelfer-ejb/src/test/java/de/erstihelfer/erstihelfer/RegisterNewUserTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import de.erstihelfer.dto.UserLoginResponse;
+import de.erstihelfer.erstihelfer.*;
 
 
 /**
@@ -25,6 +26,8 @@ public class RegisterNewUserTest {
 	
 	@EJB
 	ErstiHelferOnlineIntegration bean;
+	
+	
 	@Deployment
     public static WebArchive createDeployment() {
     	return ShrinkWrap.create(WebArchive.class, "test.war")
@@ -32,16 +35,25 @@ public class RegisterNewUserTest {
                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")               
                .addAsWebInfResource("META-INF/ejb-jar.xml", "ejb-jar.xml");
 	}
-	
-	@Test
-	/**
-	 * Prueft, ob ein neuer User erfolgreich registriert werden kann.
-	 */
-	public void testRegistration() throws Exception {
-	    UserLoginResponse loginResponse = bean.registerNewUser("Pepe", 4);
-	    assertEquals(loginResponse.getReturnCode(), 0);
-	 	int sessionId = loginResponse.getSessionId();
-		bean.logout(sessionId);
-	}
 
-}
+
+
+		/*
+		@Test
+		*//**
+		 * Prueft, ob ein neuer User erfolgreich registriert werden kann.
+		 *//*
+		public void testRegistration() throws Exception {
+		    UserLoginResponse loginResponse = bean.registerNewUser("Pepe", 4);
+		    assertEquals(loginResponse.getReturnCode(), 0);
+		 	int sessionId = loginResponse.getSessionId();
+			bean.logout(sessionId);
+		}
+	*/
+	
+	}
+	
+	
+	
+
+
