041ea0833c28018709fe7363d0db10736c368cba
TweakerBell <amaydad@gmail.com>
2016-06-18 20:52:02 +0200

    Update User.java

--- ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/entities/User.java
+++ ErstiHelfer/ErstiHelfer-ejb/src/main/java/de/erstihelfer/entities/User.java
@@ -39,9 +39,9 @@ public class User implements Serializable {
 	private int groupNr;
 
 	@ManyToMany(mappedBy="users")
-//	@JoinTable(name = "GROUP_APPOINTMENT", joinColumns = {
-//			@JoinColumn(name = "GROUPNR", referencedColumnName = "GROUPNR") }, inverseJoinColumns = {
-//					@JoinColumn(name = "APPOINTMENT_ID", referencedColumnName = "ID") })
+	@JoinTable(name = "GROUP_APPOINTMENT", joinColumns = {
+		@JoinColumn(name = "GROUPNR", referencedColumnName = "GROUPNR") }, inverseJoinColumns = {
+					@JoinColumn(name = "APPOINTMENT_ID", referencedColumnName = "ID") })
 	List<Appointment> appointments = new ArrayList<Appointment>();
 
 	public User() {
